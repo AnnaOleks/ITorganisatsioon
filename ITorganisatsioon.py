183a0648c9f088cc4a10d7b8039cdb23c91c6b78
AnnaO <annaoleks88@gmail.com>
2025-01-29 14:42:42 +0200

    Добавьте файлы проекта.

---
+++ ITorganisatsioon.py
@@ -0,0 +1,38 @@
+openOrganization="Open Organization plusses: \n 1) flexible \n 2) inovations \n 3) communication with world"
+closeOrganization="Close Organization plusses: \n 1) stability \n 2) more commerce \n 3) better security"
+
+print(openOrganization)
+print()
+print(closeOrganization)
+print()
+
+commerceOrganization="Commerce Organization: \n Examples: Sony, Mictosoft, Amazon. On voimalus saada kasu \n konkurents \n Turg"
+nocommerceOrganization="Nocommerce Organization: \n Examples: Punane rist, Greanpeace, Medicins Sans Frontieres \n Amnesty international \n pole voimalust saada kasu \n no konkurents"
+govermentOrganization="Goverment Organization: \n Examples: Jails, omavalitsused. \n Rahstamine: maksud, riigieelarve"
+educationalOrganization="Educational Organization: \n Examples: koolid, ulikoolid, toostushariduskeskused. \n Rahastamine: Riigieelarve"
+startup="Startup: \n Examples: Bolt, wise, Pipedrive \n Rahastamine: Investeeringud"
+internationalOrganization="International Organization: \n Examples: NATO, OON. \n Rahastamine: Koik liikmed maksavad maaratud tasu."
+mediaOrganization="Media Organization: \n Examples: ERR, Delfi, Postimeest. \n Rahastamine: Reklaam"
+religionOrganization="Religion Organization: \n Examples: Koik kirikud. \n Rahastamine: riigieelarve, toetused"
+associations="Associations: \n Examples: Erasmus. \n Rahastamine: Riigieelarve, investeerimine"
+fonds="Fonds: \n Examples: Bill ja Melinda Gatesi saastud. \n Rahastamine: toetused, investeerimine"
+
+print(commerceOrganization)
+print()
+print(nocommerceOrganization)
+print()
+print(govermentOrganization)
+print()
+print(educationalOrganization)
+print()
+print(startup)
+print()
+print(internationalOrganization)
+print()
+print(mediaOrganization)
+print()
+print(religionOrganization)
+print()
+print(associations)
+print()
+print(fonds)
